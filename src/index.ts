6bbe590bbb469344138eeadfb520700fa062d167
Maximillian George <maximillian.george@gmail.com>
2021-10-31 22:20:57 +0100

    works

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import Module from '../miniscript.js'
+
+export class Policy {
+  instance: any
+
+  constructor () {
+  }
+
+  public async init () {
+    this.instance = await Module()
+  }
+
+  public compile (src: string) {
+
+    const { cwrap, _malloc, _free, UTF8ToString } = this.instance
+
+    const _compile = cwrap('miniscript_compile', 'none', ['string', 'number', 'number', 'number', 'number', 'number', 'number'])
+    const msOut = _malloc(10000)
+    const costOut = _malloc(500)
+    const asmOut = _malloc(100000)
+
+    _compile(
+      src,
+      msOut,
+      10000,
+      costOut,
+      500,
+      asmOut,
+      100000
+    )
+
+    const out = {
+      ms: UTF8ToString(msOut),
+      cost: UTF8ToString(costOut),
+      asm: UTF8ToString(asmOut)
+    }
+
+    _free(msOut)
+    _free(costOut)
+    _free(asmOut)
+
+    return out
+
+  }
+
+  public analyze (src: string) {
+
+    const { _malloc, _free, UTF8ToString, cwrap } = this.instance
+
+    const _analyze = cwrap('miniscript_analyze', 'none', ['string', 'number', 'number', 'number', 'number'])
+
+    const msOut = _malloc(10000)
+    const costOut = _malloc(500)
+    const asmOut = _malloc(100000)
+
+    _analyze(
+      src,
+      msOut,
+      10000,
+      costOut,
+      500,
+      asmOut,
+      100000
+    )
+
+    const out = {
+      ms: UTF8ToString(msOut),
+      cost: UTF8ToString(costOut),
+      asm: UTF8ToString(asmOut)
+    }
+
+    _free(msOut)
+    _free(costOut)
+    _free(asmOut)
+
+    return out
+
+  }
+
+}
+
+(async () => {
+
+  const policy = new Policy()
+
+  await policy.init()
+
+  console.log(policy.compile('and(pk(A),or(pk(B),or(9@pk(C),older(1000))))'))
+  console.log(policy.analyze('and_v(v:pk(K),pk(A))'))
+
+})()
