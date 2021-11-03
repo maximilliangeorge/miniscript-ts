b11442d25d3815c2a186c178e8dc9cf4c799447f
Maximillian George <maximillian.george@gmail.com>
2021-11-03 22:23:17 +0100

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,13 @@
-export declare class Policy {
-    instance: any;
-    constructor();
-    init(): Promise<void>;
-    compile(src: string): {
-        ms: any;
-        cost: any;
-        asm: any;
-    };
-    analyze(src: string): {
-        ms: any;
-        cost: any;
-        asm: any;
-    };
-}
+declare type Policy = string;
+declare type Miniscript = string;
+export declare function compilePolicy(policy: Policy): Promise<{
+    ms: any;
+    cost: any;
+    asm: any;
+}>;
+export declare function analyzeMiniscript(miniscript: Miniscript): Promise<{
+    analyzeOut: any;
+    asmOut: any;
+}>;
+export {};
 //# sourceMappingURL=index.d.ts.map
